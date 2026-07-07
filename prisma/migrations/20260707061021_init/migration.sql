48f5b1df69deb7cb0e6bdcf1ed5e00850a2a306a
Rashed Ali <rashedalidesigner@gail.com>
2026-07-07 12:18:16 +0600

    create prisma project and create schema

---
+++ prisma/migrations/20260707061021_init/migration.sql
@@ -0,0 +1,123 @@
+-- CreateEnum
+CREATE TYPE "role" AS ENUM ('TENANT', 'LANDLORD', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "propertieStatus" AS ENUM ('AVALABLE', 'RENTED', 'UNAVALABLE');
+
+-- CreateEnum
+CREATE TYPE "rentalStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED', 'ACTIVE', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "paymentStatus" AS ENUM ('PENDING', 'COMPLETED', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "paymentProvider" AS ENUM ('CARD', 'CASH');
+
+-- CreateTable
+CREATE TABLE "Categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payment" (
+    "id" TEXT NOT NULL,
+    "rentalRequest_id" TEXT NOT NULL,
+    "transaction_id" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "paymentProvider" "paymentProvider" NOT NULL DEFAULT 'CASH',
+    "paymentStatus" "paymentStatus" NOT NULL DEFAULT 'PENDING',
+    "paidAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Properties" (
+    "id" TEXT NOT NULL,
+    "landlord_id" TEXT NOT NULL,
+    "category_id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "bedroom" INTEGER NOT NULL,
+    "bathroom" INTEGER NOT NULL,
+    "amenities" TEXT[],
+    "status" "propertieStatus" NOT NULL DEFAULT 'AVALABLE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Properties_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RentalRequest" (
+    "id" TEXT NOT NULL,
+    "tenant_id" TEXT NOT NULL,
+    "property_id" TEXT NOT NULL,
+    "status" "rentalStatus" NOT NULL DEFAULT 'PENDING',
+    "moveInDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "RentalRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" TEXT NOT NULL,
+    "tenant_id" TEXT NOT NULL,
+    "properties_id" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "role" NOT NULL DEFAULT 'TENANT',
+    "cretedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Payment_rentalRequest_id_key" ON "Payment"("rentalRequest_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Payment_transaction_id_key" ON "Payment"("transaction_id");
+
+-- AddForeignKey
+ALTER TABLE "Payment" ADD CONSTRAINT "Payment_rentalRequest_id_fkey" FOREIGN KEY ("rentalRequest_id") REFERENCES "RentalRequest"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Properties" ADD CONSTRAINT "Properties_landlord_id_fkey" FOREIGN KEY ("landlord_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Properties" ADD CONSTRAINT "Properties_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RentalRequest" ADD CONSTRAINT "RentalRequest_tenant_id_fkey" FOREIGN KEY ("tenant_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RentalRequest" ADD CONSTRAINT "RentalRequest_property_id_fkey" FOREIGN KEY ("property_id") REFERENCES "Properties"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_tenant_id_fkey" FOREIGN KEY ("tenant_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_properties_id_fkey" FOREIGN KEY ("properties_id") REFERENCES "Properties"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
